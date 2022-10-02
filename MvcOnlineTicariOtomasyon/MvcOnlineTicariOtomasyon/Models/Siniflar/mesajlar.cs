7e2b5190c02dba96a7ed98f3f060ce6ade9d5937
AhmetOZs <114857196+AhmetOZs@users.noreply.github.com>
2022-10-02 14:01:57 +0300

    OnlineTicariOtomasyon-Asp.NET-MVC

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/mesajlar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class mesajlar
+    {
+        [Key]
+        public int MesajID { get; set; }
+        [Column(TypeName = "VarChar")]
+        [StringLength(50)]
+        public string Kimden { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(50)]
+        public string Kime { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(50)]
+        public string Baslik { get; set; }
+
+        [Column(TypeName = "VarChar")]
+        [StringLength(2000)]
+        public string Mesaj { get; set; }
+
+        [Column(TypeName = "Smalldatetime")]
+        public DateTime Tarih { get; set; }
+
+    }
+}
